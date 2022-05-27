c66363ad05ee7360eb56858f6ce000baa4030bec
Leonardo <josue.aguilar1995@gmail.com>
2022-05-27 12:10:33 -0500

    completed propmtStaff()

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ const promptManager = () => {
     {
       type: "input",
       name: "managerId",
-      message: "What is the team manager's employee ID?",
+      message: "What is the team manager's employee ID number?",
       validate: (managerIdInput) => {
         if (managerIdInput) {
           return true;
         } else {
-          console.log("Please enter the team manager's ID.");
+          console.log("Please enter the team manager's ID number.");
           return false;
         }
       },
@@ -71,24 +71,164 @@ const promptStaff = (teamData) => {
     .prompt([
       {
         type: "list",
-        name: "addStaff",
+        name: "staffType",
         message: "Would you like to add an ENGINEER, INTERN, or FINISH?",
         choices: ["Engineer", "Intern", "Finish"],
       },
       {
         type: "input",
-        name: "staffName",
-        message: "What is your staff member's NAME?",
-        validate: (staffName) => {
-          if (staffName) {
+        name: "engineerName",
+        message: "What is the engineer's NAME?",
+        validate: (engineerNameInput) => {
+          if (engineerNameInput) {
             return true;
           } else {
-            console.log("Please enter in the staff member's NAME.");
+            console.log("Please enter in the engineer's NAME.");
             return false;
           }
         },
-        when: ({ addStaff }) => {
-          if (addStaff !== "Finish") {
+        when: ({ staffType }) => {
+          if (staffType == "Engineer") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is the engineer's ID number?",
+        validate: (engineerIdInput) => {
+          if (engineerIdInput) {
+            return true;
+          } else {
+            console.log("Please enter in the engineer's ID number.");
+            return false;
+          }
+        },
+        when: ({ staffType }) => {
+          if (staffType == "Engineer") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineer's EMAIL address?",
+        validate: (engineerEmailInput) => {
+          if (engineerEmailInput) {
+            return true;
+          } else {
+            console.log("Please enter in the engineer's EMAIL address.");
+            return false;
+          }
+        },
+        when: ({ staffType }) => {
+          if (staffType == "Engineer") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is the engineer's GITHUB username?",
+        validate: (engineerGithubInput) => {
+          if (engineerGithubInput) {
+            return true;
+          } else {
+            console.log("Please enter in the engineer's GITHUB username.");
+            return false;
+          }
+        },
+        when: ({ staffType }) => {
+          if (staffType == "Engineer") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "internName",
+        message: "What is the intern's NAME?",
+        validate: (internNameInput) => {
+          if (internNameInput) {
+            return true;
+          } else {
+            console.log("Please enter in the intern's NAME.");
+            return false;
+          }
+        },
+        when: ({ staffType }) => {
+          if (staffType == "Intern") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "What is the intern's ID number?",
+        validate: (internIdInput) => {
+          if (internIdInput) {
+            return true;
+          } else {
+            console.log("Please enter in the intern's ID number.");
+            return false;
+          }
+        },
+        when: ({ staffType }) => {
+          if (staffType == "Intern") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is the intern's EMAIL address?",
+        validate: (internEmailInput) => {
+          if (internEmailInput) {
+            return true;
+          } else {
+            console.log("Please enter in the intern's EMAIL address.");
+            return false;
+          }
+        },
+        when: ({ staffType }) => {
+          if (staffType == "Intern") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What is the name of the intern's SCHOOL?",
+        validate: (internSchoolInput) => {
+          if (internSchoolInput) {
+            return true;
+          } else {
+            console.log("Please enter in the name of the intern's SCHOOL.");
+            return false;
+          }
+        },
+        when: ({ staffType }) => {
+          if (staffType == "Intern") {
             return true;
           } else {
             return false;
@@ -98,9 +238,13 @@ const promptStaff = (teamData) => {
     ])
     .then((staffData) => {
       teamData.staff.push(staffData);
-      if (teamData.staff[teamData.staff.length - 1].addStaff !== "Finish") {
+
+      // if the last entry in staff[] has an staffType value of 'Finish', end loop
+      if (teamData.staff[teamData.staff.length - 1].staffType !== "Finish") {
         return promptStaff(teamData);
       } else {
+        // get rid of last 'Finish' value in array and then return array
+        teamData.staff.pop();
         return teamData;
       }
     });
