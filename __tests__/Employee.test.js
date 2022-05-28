e8bef3ff01fc64deb52119b2e7f3f100614e1e64
Leonardo <josue.aguilar1995@gmail.com>
2022-05-28 08:44:25 -0500

    created Employee tests and class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,27 @@
+const Employee = require("../lib/Employee.js");
+
+test("creates an employee object", () => {
+  const employee = new Employee("Jeff", "69", "jeff69@email.com");
+
+  expect(employee.name).toBe("Jeff");
+  expect(employee.id).toBe("69");
+  expect(employee.email).toBe("jeff69@email.com");
+});
+
+test("gets the employee's name", () => {
+  const employee = new Employee("Jeff", "69", "jeff69@email.com");
+
+  expect(employee.getName()).toEqual("Jeff");
+});
+
+test("gets employee's email", () => {
+  const employee = new Employee("Jeff", "69", "jeff69@email.com");
+
+  expect(employee.getEmail()).toEqual("jeff69@email.com");
+});
+
+test("gets employee's employee's role", () => {
+  const employee = new Employee("Jeff", "69", "jeff69@email.com");
+
+  expect(employee.getRole()).toEqual("Employee");
+});
